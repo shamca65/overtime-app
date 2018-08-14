2c30bad5fdcf533b01644c6b3952351188cb3fc9
Shaun McArthur <shamca@bell.net>
2018-08-14 16:30:58 -0400

    cleaned up post controller a bit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,38 @@
 class PostsController < ApplicationController
+  before_action :set_post, only:[:show]
+
   def index
 
   end
+
+  def new
+    # just renders the form  and create the instance variable
+    @post = Post.new
+  end
+
+  def create
+    # whitelist params
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Your post was created successfully'
+    else
+
+      render :new, notice: 'Post was not saved'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
+
 end
