bd0a618e56e30a332660fd46437fd56803efa227
Shaun McArthur <shamca@bell.net>
2018-09-12 16:28:35 -0400

    added tests for admin log index page

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -5,9 +5,25 @@ describe 'AuditLog Feature' do
   let(:audit_log) {FactoryBot.create(:audit_log)}
 
   describe '#index' do
+
+    before do
+      admin_user = FactoryBot.create(:admin_user)
+      login_as(admin_user, :scope => :user)
+    end
+
     it 'has an index page that can be reached' do
       visit audit_logs_path
-      expect(page.status.code).to eq(200)
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'renders audit log content' do
+      FactoryBot.create(:audit_log)
+      visit audit_logs_path
+      expect(page).to have_content("SNOW, JON")
+    end
+
+    xit 'cannot be accessed by non-admin users' do
+
     end
   end
 
