d8e2a8d7f62c22e27b2de0d285060f4fb374042f
Shaun McArthur <shamca65@gmail.com>
2018-10-06 19:40:36 -0400

    added callback for confirming overtime status

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -7,8 +7,13 @@ class AuditLog < ActiveRecord::Base
   validates_presence_of :user_id, :status, :start_date
 
   after_initialize :set_defaults
+  before_update :set_end_date :confirmed?
 
   private
+
+  def set_end_date
+    self.end_date = Date.today
+  end
 
   def set_defaults
     self.start_date ||= Date.today - 6.days
